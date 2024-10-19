f4c1b1e31249506001dec0e44d0cf272da99b7f7
admin <oybeks.pro@gmail.com>
2024-10-19 19:29:32 +0500

    update  handlers

--- src/main/java/revolusion/hotelmanagement/config/handler/GlobalExceptionHandler.java
+++ src/main/java/revolusion/hotelmanagement/config/handler/GlobalExceptionHandler.java
@@ -4,8 +4,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import revolusion.hotelmanagement.dto.AppErrorDTO;
 
 import java.time.LocalDateTime;
@@ -33,5 +35,27 @@ public class GlobalExceptionHandler {
                 .timestamp(LocalDateTime.now())
                 .build();
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGlobalException(Exception ex, HttpServletRequest request) {
+        AppErrorDTO error = AppErrorDTO.builder()
+                .errorMessage(ex.getMessage())
+                .errorCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .errorPath(request.getRequestURI())
+                .timestamp(LocalDateTime.now())
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        AppErrorDTO error = AppErrorDTO.builder()
+                .errorMessage(ex.getMessage())
+                .errorCode(HttpStatus.BAD_REQUEST.value())
+                .errorPath(request.getRequestURI())
+                .timestamp(LocalDateTime.now())
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
